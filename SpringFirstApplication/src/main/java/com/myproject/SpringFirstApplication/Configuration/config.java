8587a57d93b772d1f2a411797b0be95e09de57f2
Raghav Garg <grraghav120@gmail.com>
2024-08-15 11:38:51 +0530

    remove unused depenency

--- SpringFirstApplication/src/main/java/com/myproject/SpringFirstApplication/Configuration/config.java
+++ SpringFirstApplication/src/main/java/com/myproject/SpringFirstApplication/Configuration/config.java
@@ -8,7 +8,6 @@ import com.myproject.SpringFirstApplication.Service.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.ui.Model;
 
 @Configuration
 public class config {
